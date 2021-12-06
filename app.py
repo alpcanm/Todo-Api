a7d363957b545cfd779d3e8e99060b05fbb32e6a
Alp Can <alpcanm@gmail.com>
2021-12-06 03:53:25 +0300

    temel düzeylde get ve post metodları çalıştırıldı

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
+from core.routes.notes.note_book import NoteBook
 from core.routes.users import User
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
@@ -8,10 +11,17 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/users", methods=['GET', 'POST','PUT'])
+# @app.route("/users", methods=['GET', 'POST', 'PUT'])
+# def users():
+#     user = User(request.method)
+#     return user.switch()
+
+
+@app.route("/notebooks", methods=['GET', 'POST', 'PUT'])
 def users():
-    user = User(request.method)
-    return user.switch()
+    
+    noteBook = NoteBook(request)
+    return noteBook.switch()
 
 
 if __name__ == "__main__":
--- core/database/connection.py
+++ core/database/connection.py
@@ -4,7 +4,7 @@ import psycopg2
 class MyDb:
     def __init__(self):
         self.conn = psycopg2.connect(
-            database="mydb", user='postgres', password='localHost123', host='localhost', port='5432')
+            database="db_todo", user='postgres', password='localHost123', host='localhost', port='5432')
         self.conn.autocommit = True
         self.cursor = self.conn.cursor()
 
@@ -12,3 +12,15 @@ class MyDb:
         sql = '''Create  Into mydb'''
         self.cursor.execute(sql)
         self.conn.close()
+        
+    def post(self,query:str ):
+        print(query)
+        self.cursor.execute(query)
+        self.conn.close()
+        
+    def get(self,query:str ):
+        print(query)
+        self.cursor.execute(query)
+        #self.conn.close()
+        return self.cursor.fetchall()
+        
